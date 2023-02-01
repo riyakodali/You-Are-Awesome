b6bb5b2f2f866c5f2c7770510c5aa6a362a0262c
Riya Kodali <riyakodali@gmail.com>
2023-02-01 13:37:02 -0500

    Week 3, Ch 1.14 Randomization completed

--- You-Are-Awesome/ContentView.swift
+++ You-Are-Awesome/ContentView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Riya Kodali on 1/29/23.
 //
-
+//test
 import SwiftUI
 
 struct ContentView: View {
@@ -49,17 +49,13 @@ struct ContentView: View {
                                     "Fabulous?, That's You!",
                                     "You Make Me Smile!", "When the Genius Bar Needs Help, They Call You!"]
                     
-                   messageString = messages[messageNumber]
+                    messageString = messages[Int.random(in: 0...messages.count-1)]
                     messageNumber += 1
                     if messageNumber == messages.count {
                         messageNumber = 0
                     }
                     
-                    imageName = "image\(imageNumber)"
-                    imageNumber = imageNumber + 1
-                    if imageNumber > 9 {
-                        imageNumber = 0
-                    }
+                    imageName = "image\(Int.random(in: 0...9))"
                     print(imageNumber)
                 }
                 .buttonStyle(.borderedProminent)
